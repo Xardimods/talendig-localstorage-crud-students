3e533d606b1dea45e9c497a39e797a5c9bdd57b3
Enyel Liranzo <xardimodschannel@gmail.com>
2024-02-10 00:59:24 -0400

    [update] Updating some changes

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,21 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.getElementById("btn");
+const button = document.getElementById("btn");
 
-  button.addEventListener("click", () => {
-    const dataStuents = {
-      id: generateUniqueId(),
-      student_first_name: document.getElementById("firstname").value,
-      student_last_name: document.getElementById("lastname").value,
-      student_id: document.getElementById("id").value,
-      student_course: document.getElementById("course").value,
-      student_note: document.getElementById("note").value,
-    };
+button.addEventListener("click", () => {
+  const dataStuents = {
+    id: generateUniqueId(),
+    student_first_name: document.getElementById("firstname").value,
+    student_last_name: document.getElementById("lastname").value,
+    student_id: document.getElementById("id").value,
+    student_course: document.getElementById("course").value,
+    student_note: document.getElementById("note").value,
+  };
 
-    const uniqueId = generateUniqueId();
-    const JSONStudentsData = JSON.stringify(dataStuents);
+  const uniqueId = generateUniqueId();
+  const JSONStudentsData = JSON.stringify(dataStuents);
 
-    localStorage.setItem(uniqueId, JSONStudentsData);
-  });
+  localStorage.setItem(uniqueId, JSONStudentsData);
 });
 
 function generateUniqueId() {
